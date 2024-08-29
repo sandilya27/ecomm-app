c0e8117b6d03a6c9faae3182d4e25e8fe9a50270
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-08-29 17:54:04 +0530

    created the user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    lowercase: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters long"],
+  },
+  cartItems: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+        required: true,
+      },
+    },
+  ],
+  role: {
+    type: String,
+    enum: ["coustmer", "admin"],
+    default: "coustmer",
+  },
+},{
+    timestamps: true,
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Hash the password before saving the user model
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+// Compare user password
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+export default User;
