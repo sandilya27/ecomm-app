77fc5a11aeee1ebff7466c3c7528956fe7046231
c s sachindra <113860300+sandilya27@users.noreply.github.com>
2024-08-29 17:41:40 +0530

    database connection

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,17 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.route.js";
 
+import { connectDB } from "./lib/db.js";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use("/api/auth",authRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
+  connectDB();
 });
